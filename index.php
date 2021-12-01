447dde7aa42e5645394a043025be164a7e549c66
willandru <willandru@hotmail.com>
2021-11-30 19:28:16 -0500

    Getting something from PHP

---
+++ index.php
@@ -0,0 +1,17 @@
+<h1> Codigo HTML </h1>
+
+<?php
+
+    #Esto es un comentario en php
+    // Este es otro tipo de comentario en php
+    /* Estas
+    son vairas lineas
+    de comentario en <php></php> */
+
+    echo "<h1> Codigo PHP </h1>";
+    print("Hola Mundo PHP");
+    echo "<---Comentario---> ";
+    
+    
+
+?>
